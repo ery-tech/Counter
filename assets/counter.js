95c0f87bbec5c448e7e872db6b318d640ad3e8ba
Ery-tech <erikaguardo99@gmail.com>
2023-01-31 11:17:24 +0100

    feedbacks

--- assets/counter.js
+++ assets/counter.js
@@ -1,88 +1,91 @@
 //constructor with functions for dom elements
 const createDomElement = (tag, classes, content) => {
-    const el = document.createElement(tag);
-    classes.forEach(e => el.classList.add(e));
-    el.innerHTML= content;
-    return el;
-  }
-  //article
-  const rootElement = document.querySelector('#body');
-  const newElement = 
-  createDomElement('article' , ['main'],"Hi, feel free to use the counter!");
-  rootElement.appendChild(newElement);
-  //counter
-  const main = document.querySelector('.main');
-  const newElement2 = createDomElement('div',['counter']," " );
-  main.append(newElement2);
-  //counter-board
-  const counter = document.querySelector('.counter');
-  const paragraph = createDomElement('p',['counter-board'],'0');
-  counter.append(paragraph);
+  const el = document.createElement(tag);
+  classes.forEach((e) => el.classList.add(e));
+  el.innerHTML = content;
+  return el;
+};
+//article
+const rootElement = document.querySelector("#body");
+const article = createDomElement(
+  "article",
+  ["main"],
+  "Hi, feel free to use the counter!"
+);
+rootElement.appendChild(article);
+//counter
+const main = document.querySelector(".main");
+const counterDiv = createDomElement("div", ["counter"], " ");
+main.append(counterDiv);
+//counter-board
+const counter = document.querySelector(".counter");
+const paragraph = createDomElement("p", ["counter-board"], "0");
+counter.append(paragraph);
 //buttons-section
-  const newElement3 = createDomElement('div',['buttons-section'],'');
-  main.append(newElement3);
+const buttonsDiv = createDomElement("div", ["buttons-section"], "");
+main.append(buttonsDiv);
 //three buttons
-  const buttonsSection = document.querySelector('.buttons-section');
-  const button1 = createDomElement('button',['decrease-btn'],'-');
-  const button2 = createDomElement('button', ['reset-btn'],'Reset');
-  const button3 = createDomElement('button',['increase-btn'],'+');
-  buttonsSection.append(button1,button2,button3);
+const buttonsSection = document.querySelector(".buttons-section");
+const BUTTON_MINUS = createDomElement("button", ["decrease-btn"], "-");
+const BUTTON_RESET = createDomElement("button", ["reset-btn"], "Reset");
+const BUTTON_PLUS = createDomElement("button", ["increase-btn"], "+");
+buttonsSection.append(BUTTON_MINUS, BUTTON_RESET, BUTTON_PLUS);
 
- //adding style to dom elements
-  function css(element, style) {
-    for (const property in style)
-        element.style[property] = style[property];
+//adding style to dom elements
+function updateCss(element, style) {
+  for (const property in style) element.style[property] = style[property];
 }
 
-css(main,{ 'color':'navy',
-  'font-family': "'Franklin Gothic Medium', 'Arial Narrow'",
-  'font-weight' : 'normal',
-  'background': 'linear-gradient(90deg, #FC466B 0%, #3F5EFB 100%)',
-    'height' : '100vh',
-    'display' : 'flex',
-    'flex-direction' : 'column',
-    'justify-content': 'center',
-    'align-items'   : 'center'
-})
-css(paragraph,{
-   'font-size': '30px'
-})
+updateCss(main, {
+  color: "navy",
+  "font-family": "'Franklin Gothic Medium', 'Arial Narrow'",
+  "font-weight": "normal",
+  background: "linear-gradient(90deg, #FC466B 0%, #3F5EFB 100%)",
+  height: "100vh",
+  display: "flex",
+  "flex-direction": "column",
+  "justify-content": "center",
+  "align-items": "center",
+});
+updateCss(counter, {
+  "font-size": "30px",
+});
+updateCss(paragraph, {
+  "padding-top": "2rem",
+});
 
+updateCss(buttonsSection, {
+  display: "flex",
+  "justify-content": "center",
+  "align-items": "center",
+  "padding-top": "2rem",
+  "padding-bottom": "2rem",
+});
 
-
-
-css(newElement2,{
-  'display': 'flex',
-  'justify-content' : 'center',
-  'align-items': 'center',
-  'padding-top' : '2rem',
-  'padding-bottom' : '2rem'
-})
-  
-//get the increase and decrease button 
-const incrementButton = document.querySelector('.increase-btn');
-const decrementButton = document.querySelector('.decrease-btn');
-const resetButton = document.querySelector('.reset-btn');
-//get the counter-board value 
-const counterBoard = document.querySelector('.counter-board');
+//get the counter-board value
+const COUNTER_BOARD = document.querySelector(".counter-board");
 //variable holding the counter value
-let counterValue = 0;
+let COUNTER_VALUE = 0;
 // function that increases the counter by 1
-const increaseCounter = () =>{
-  counterBoard.innerHTML = ++counterValue;
-}
+const INCREASE_COUNTER = () => {
+  COUNTER_BOARD.innerHTML = ++COUNTER_VALUE;
+};
 // function that decreases the counter by 1
-const decreaseCounter = () =>{
-  counterBoard.innerHTML = --counterValue;
-}
+const DECREASE_COUNTER = () => {
+  COUNTER_BOARD.innerHTML = --COUNTER_VALUE;
+};
 //reset functionality
-const resetCounter = () =>{
-  
-  counterBoard.innerHTML = 0;
-  counterValue = 0;
-}
-// Add event listener to the buttons
-incrementButton.addEventListener('click', increaseCounter);
-decrementButton.addEventListener('click', decreaseCounter);
-resetButton.addEventListener('click', resetCounter);
-
+const RESET_COUNTER = () => {
+  COUNTER_BOARD.innerHTML = 0;
+  COUNTER_VALUE = 0;
+};
+// Added event listener to the buttons with event delegation
+buttonsSection.addEventListener("click", (event) => {
+  if (event.target.matches(".increase-btn")) {
+    INCREASE_COUNTER();
+  } else if (event.target.matches(".decrease-btn")) {
+    DECREASE_COUNTER();
+  } else if (event.target.matches(".reset-btn")) {
+    RESET_COUNTER();
+  }
+});
